8f2f32537eb86a3f5d1d25406aeae0459a71d45d
nafisaali0 <nafisaali20006@gmail.com>
2025-06-29 02:55:28 +0600

    change theme to light

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -6,17 +8,20 @@ export default {
       light_purple: "#6a61d1",
       light_gray: "#dbdbde",
       white: "#FFFFFF",
-      black: "#000000",      
+      black: "#000000",
       card_white: "#f3f3f3",
-      hover_btn:"#302ba7",
-      body_color:"#e7e7e9",     
+      hover_btn: "#302ba7",
+      body_color: "#e7e7e9",
       hover_gray: "#eeeeee",
       dash_nav: "#f5f1fe",
       purple: "#c3bef0",
-      icon_gray:"#9ba6a5"
-      
+      icon_gray: "#9ba6a5",
     },
     extend: {},
   },
-  plugins: [require("daisyui")],
+  // plugins: [require("daisyui")],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light"],
+  },
 };
