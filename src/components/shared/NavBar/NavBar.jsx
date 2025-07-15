9605bd0860b17ecfc8ed5abc5de4d9c811d1f871
nafisaali0 <nafisaali20006@gmail.com>
2025-07-15 08:04:08 +0600

    add padding in nav container

--- src/components/shared/NavBar/NavBar.jsx
+++ src/components/shared/NavBar/NavBar.jsx
@@ -143,7 +143,7 @@ const NavBar = () => {
             </div> */}
 
             <div className="fixed top-0 w-full z-50 px-5bg-white/90 backdrop-blur-sm shadow-md">
-                <div className="navbar">
+                <div className="navbar px-5">
                     <div className="navbar-start md:space-x-5">
                         <div tabIndex={0} role="button">
                             <div className="w-16">
