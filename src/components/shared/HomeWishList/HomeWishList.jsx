ae04ff517eaca9b4133f49804a1c3890289eba2f
nafisaali0 <nafisaali20006@gmail.com>
2025-07-21 13:06:37 +0600

    change wishlist

--- src/components/shared/HomeWishList/HomeWishList.jsx
+++ src/components/shared/HomeWishList/HomeWishList.jsx
@@ -30,56 +30,34 @@ const HomeWishList = () => {
                     {
                         sortRecentSaveBlogs.slice(0, 2).map(saveBlogs =>
                             <>
-                                {/* <div className=" bg-card_white p-5 rounded">
-                                    <div className="flex items-center gap-4">
-                                        <div className="avatar-group -space-x-6 rtl:space-x-reverse">
-                                            <div className="avatar w-9">
-                                                <img src={saveBlogs.owner_image} />
-                                            </div>
-                                        </div>
-                                        <div>
-                                            <span className="font-semibold">{saveBlogs.owner_name}</span>
-                                        </div>
-                                    </div>
-                                    <Link to={`/blogdetails/${saveBlogs.blogId}`} target="_blank">
-                                        <h1 className="text-2xl font-bold my-3 hover:text-light_purple">{saveBlogs.title}</h1>
-                                    </Link>
-                                    <div className="flex gap-4 items-center my-5">
-                                        <span>{saveBlogs.saveDate}</span>
-                                        <span>{saveBlogs.saveTime}</span>
-                                    </div>
-                                </div> */}
-
-                                {/* new card */}
-
                                 <div className="flex flex-col justify-start">
                                     <div className="relative flex flex-col md:space-x-3 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 border border-borderColour bg-mainTheme">
-                                        <div className="w-full bg-[#ffffffE6] flex flex-col space-y-2 p-3">
+                                        <div className="w-full bg-mainTheme flex flex-col space-y-2 p-3">
                                             <div className="flex items-center">
                                                 <img className="object-cover h-5 w-5 rounded-full" src={saveBlogs.owner_image} alt="Avatar" />
                                                 <Link to={`/blogs/bloggeremail/${saveBlogs.owner_Email}`}>
-                                                    <span className="mx-2 md:text-sm text-[#6B6B6B] text-base font-medium">{saveBlogs.owner_name}</span>
+                                                    <span className="mx-2 text-sm text-textSmallGray font-medium">{saveBlogs.owner_name}</span>
                                                 </Link>
                                             </div>
                                             <Link to={`/blogdetails/${saveBlogs.blogId}`}>
-                                                <h3 className="font-black text-[#000000] md:text-2xl text-xl">{saveBlogs.title}</h3>
+                                                <h3 className="font-bold text-black text-xl">{saveBlogs.title}</h3>
                                             </Link>
                                             <div className="flex items-center">
                                                 <BiSolidCategory title="category" className="text-[#6B6B6B]" style={{ width: '20px', height: '20px' }} />
-                                                <span className="mx-2 md:text-sm text-[#6B6B6B] font-medium">{saveBlogs.category}</span>
+                                                <span className="mx-2 text-sm text-textSmallGray font-medium">{saveBlogs.category}</span>
                                             </div>
                                             <div className="flex justify-between items-center">
-                                                <p className="md:text-sm text-[#6B6B6B] text-base font-medium md:block">{saveBlogs.date}</p>
+                                                <p className="text-textSmallGray text-sm font-medium md:block">{saveBlogs.date}</p>
                                                 <div className="flex space-x-2 item-center">
                                                     <div className="flex items-center">
                                                         <LikeFunctionality id={saveBlogs._id}></LikeFunctionality>
-                                                        <span className="md:text-sm text-[#6B6B6B] text-base font-medium ml-1">
+                                                        <span className="text-sm text-[#6B6B6B] font-medium ml-1">
                                                             <TotalLikes id={saveBlogs.blogId}></TotalLikes>
                                                         </span>
                                                     </div>
                                                     <div className="flex items-center">
-                                                        <AiOutlineComment title="comments" className="text-[#6B6B6B]" style={{ width: '20px', height: '20px' }} />
-                                                        <span className="md:text-sm text-[#6B6B6B] text-base font-medium ml-1">
+                                                        <AiOutlineComment title="comments" className="text-textSmallGray" style={{ width: '20px', height: '20px' }} />
+                                                        <span className="text-sm text-textSmallGray font-medium ml-1">
                                                             <BlogTotalComments id={saveBlogs.blogId}></BlogTotalComments>
                                                         </span>
                                                     </div>
