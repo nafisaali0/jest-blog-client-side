02ae06ca1a9df7c8de54065f2c4f83afa1d8a036
nafisaali0 <nafisaali20006@gmail.com>
2025-07-24 02:53:18 +0600

    change color of BlogsShow.jsx

--- src/components/shared/TabHome/BlogsShow.jsx
+++ src/components/shared/TabHome/BlogsShow.jsx
@@ -96,44 +96,44 @@ const BlogsShow = ({ blog }) => {
                     <div className="p-2 sm:w-1/2">
                         <PhotoProvider>
                             <PhotoView src={details_image}>
-                                <img className="rounded object-cover w-full h-60" src={details_image} alt="blogImage" />
+                                <img className="rounded-xl object-cover w-full h-60" src={details_image} alt="blogImage" />
                             </PhotoView>
                         </PhotoProvider>
                     </div>
-                    <div className="w-full md:w-2/3 bg-[#ffff] flex flex-col space-y-2 p-3">
+                    <div className="w-full md:w-2/3 bg-mainTheme flex flex-col space-y-2 p-3">
                         <div className="flex items-center">
                             <img className="object-cover h-5 w-5 rounded-full" src={owner_image} alt="Avatar" />
                             <Link to={`/blogs/bloggeremail/${owner_Email}`}>
-                                <span href="#" className="mx-2 md:text-sm text-[#6B6B6B] text-base font-medium">{owner_name}</span>
+                                <span className="mx-2 text-textSmallGray text-sm font-medium">{owner_name}</span>
                             </Link>
                         </div>
                         <Link to={`/blogdetails/${_id}`}>
-                            <h3 className="font-black text-[#000000] md:text-2xl text-xl">{title}</h3>
-                            <p className="md:text-sm text-[#6B6B6B] text-base font-medium">{short_description}</p>
+                            <h3 className="font-black text-black md:text-2xl text-xl">{title}</h3>
+                            <p className="text-sm text-textSmallGray font-medium">{short_description}</p>
                         </Link>
                         <div className="flex items-center">
-                            <BiSolidCategory title="category" className="text-[#6B6B6B]" style={{ width: '20px', height: '20px' }} />
-                            <span href="#" className="mx-2 md:text-sm text-[#6B6B6B] font-medium">{category}</span>
+                            <BiSolidCategory title="category" className="text-textSmallGray" style={{ width: '20px', height: '20px' }} />
+                            <span href="#" className="mx-2 text-sm text-textSmallGray font-medium">{category}</span>
                         </div>
                         <div className="flex md:flex-row justify-between">
                             <div className="flex flex-1 flex-row space-x-3 item-center">
-                                <p className="md:text-sm text-[#6B6B6B] text-base font-medium md:block">{date}</p>
+                                <p className="text-sm text-textSmallGray font-medium md:block">{date}</p>
                                 <div className="flex justify-center space-x-1">
                                     <LikeFunctionality id={_id}></LikeFunctionality>
-                                    <span className="md:text-sm text-[#6B6B6B] text-base font-medium ml-1">
+                                    <span className="text-sm text-textSmallGray font-medium ml-1">
                                         <TotalLikes id={_id}></TotalLikes>
                                     </span>
                                 </div>
                                 <div className="flex justify-center space-x-1">
-                                    <AiOutlineComment title="comments" className="text-[#6B6B6B]" style={{ width: '20px', height: '20px' }} />
-                                    <span className="md:text-sm text-[#6B6B6B] text-base font-medium ml-1">
+                                    <AiOutlineComment title="comments" className="text-textSmallGray" style={{ width: '20px', height: '20px' }} />
+                                    <span className="text-sm text-textSmallGray font-medium ml-1">
                                         <BlogTotalComments id={_id}></BlogTotalComments>
                                     </span>
                                 </div>
                             </div>
                             {/* Wishlist */}
                             <div>
-                                <BsBookmarkCheck title="save" onClick={handleWishList} className="text-[#6B6B6B] cursor-pointer" style={{ width: '20px', height: '20px' }} />
+                                <BsBookmarkCheck title="save" onClick={handleWishList} className="text-textSmallGray cursor-pointer" style={{ width: '20px', height: '20px' }} />
                             </div>
                         </div>
                     </div>
