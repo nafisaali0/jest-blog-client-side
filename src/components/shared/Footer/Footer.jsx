d02c91469113243ed6c6fbc579bef7454a958baf
nafisaali0 <nafisaali20006@gmail.com>
2025-07-26 12:33:45 +0600

    update footer

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,49 +1,57 @@
 import logo from '../../../assets/image/logo/upLogo13edit.png'
-
+import moment from 'moment';
 
 const Footer = () => {
     return (
         <>
-            <div className='bg-black px-5 lg-px-0 '>
-                <div className='container mx-auto py-10'>
+            <div className='bg-black'>
+                <div className='max-w-[1300px] mx-auto bg-black px-2 lg:px-5 py-5'>
                     <div className='flex flex-col justify-start items-start py-5'>
                         <img className='w-24' src={logo} alt="" />
                     </div>
                     <footer className="footer py-5 bg-black text-white">
-                        <div className='text-white'>
-                            <h6 className="text-light_blue">Features</h6>
-                            <a className="link link-hover">Create Blog</a>
-                            <a className="link link-hover">Manage Blog</a>
-                            <a className="link link-hover">Add Reading List</a>
-                            <a className="link link-hover">Like & Comment</a>
+                        <div className='text-sm font-medium text-mainTheme'>
+                            <h6 className="font-bold text-lg text-white">Features</h6>
+                            <span className="link link-hover">Create & Update Blog</span>
+                            <span className="link link-hover">Manage Blog</span>
+                            <span className="link link-hover">Reading List</span>
+                            <span className="link link-hover">Search By Category</span>
                         </div>
-                        <div className='text-white'>
-                            <h6 className="text-light_blue">Features</h6>
-                            <a className="link link-hover">Search Blogs</a>
-                            <a className="link link-hover">Follow Blogger</a>
-                            <a className="link link-hover">Profile Settings</a>
+                        <div className='text-sm font-medium text-mainTheme'>
+                            <h6 className="font-bold text-lg text-white">Features</h6>
+                            <span className="link link-hover">Search Blogs</span>
+                            <span className="link link-hover">Follow Blogger</span>
+                            <span className="link link-hover">Profile Settings</span>
+                            <span className="link link-hover">Like & Comment</span>
                         </div>
-                        <div className='text-white'>
-                            <h6 className="text-light_blue">Pages</h6>
-                            <a className="link link-hover">Home</a>
-                            <a className="link link-hover">Add Blogs</a>
-                            <a className="link link-hover">Details Blog</a>                           
+                        <div className='text-sm font-medium text-mainTheme'>
+                            <h6 className="font-bold text-lg text-white">Pages</h6>
+                            <a href='http://localhost:5173/' className="link link-hover">Home</a>
+                            <a href='http://localhost:5173/addblog' className="link link-hover">Create Blogs</a>
+                            <a href='http://localhost:5173/wishlist' className="link link-hover">Reading List</a>
+                            <a href='http://localhost:5173/dashboard' className="link link-hover">Dashboard</a>
                         </div>
-                        <div className='text-white'>
-                            <h6 className="text-light_blue">Pages</h6>                           
-                            <a className="link link-hover">Feature Blog</a>
-                            <a className="link link-hover">WhishList</a>
-                            <a className="link link-hover">Dashboard</a>
+                        <div className='text-sm font-medium text-mainTheme'>
+                            <h6 className="font-bold text-lg text-white">Pages</h6>
+                            <a href='http://localhost:5173/featureblog' className="link link-hover">Feature Blog</a>
+                            <a href='http://localhost:5173/dashboard/profile' className="link link-hover">User Profile</a>
+                            <a href='http://localhost:5173/dashboard/profile-setting' className="link link-hover">Settings</a>
                         </div>
-                        <div className='text-white'>
-                            <h6 className="text-light_blue">Solution</h6>
-                            <a className="link link-hover">Education</a>
-                            <a className="link link-hover">Life Knowledge</a>
+                        <div className='text-sm font-medium text-mainTheme'>
+                            <h6 className="font-bold text-lg text-white">Solution</h6>
+                            <span className="link link-hover">Education</span>
+                            <span className="link link-hover">Life Knowledge</span>
                         </div>
                     </footer>
-                    <footer className="footer border-t border-light_purple py-5 bg-black text-white">
+                    <footer className="footer border-t border-light_purple py-5 bg-black text-sm font-medium text-mainTheme">
                         <aside className="items-center grid-flow-col">
-                            <p>© 2024 JEST BLOG.<br />Created by Nafisa Ali</p>
+                            <p>© {moment().format("YYYY")} JEST BLOG.<br />
+                                Created by
+
+                                <span className='hover:text-primaryColor'>
+                                    <a href="https://github.com/nafisaali0"> Nafisa Ali</a>
+                                </span>
+                            </p>
                         </aside>
                     </footer>
                 </div>
