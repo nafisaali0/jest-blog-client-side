4b36924c803b9b67c294147b2a3586587cda93ba
nafisaali0 <nafisaali20006@gmail.com>
2025-08-01 21:04:36 +0600

    change categoryBlogs and delete one comp

--- src/components/page/CategoryBlogs/CategoryBlogs.jsx
+++ src/components/page/CategoryBlogs/CategoryBlogs.jsx
@@ -1,6 +1,4 @@
-import { useLoaderData, useLocation } from "react-router-dom";
-// import ShowCategoryBlogs from "./ShowCategoryBlogs";
-import { BiSolidCategory } from "react-icons/bi";
+import { Link, useLoaderData, useLocation } from "react-router-dom";
 import LikeFunctionality from "../../shared/LikeFunctionality/LikeFunctionality";
 import TotalLikes from "../../shared/LikeFunctionality/TotalLikes";
 import { AiOutlineComment } from "react-icons/ai";
@@ -16,28 +14,28 @@ const CategoryBlogs = () => {
     const location = useLocation();
     const parts = location.pathname.split('/');
     const path = decodeURIComponent(parts[3]);
-    console.log(path)
-    
+    // console.log(path)
+
 
     return (
         <>
             <div className="my-10">
-                <div className="flex justify-center items-center mb-14">
-                    <h1 className="text-3xl font-bold text-black">{path}</h1>
+                <div className="flex justify-center items-center mb-10">
+                    <h1 className="text-5xl font-bold text-black">{path}</h1>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 lg:gap-4 gap-2">
                     {
                         blogs.map(blog =>
                             <>
-                                <div className="card bg-mainTheme hover:shadow-xl space-y-2 h-96">
+                                <div className="card space-y-2 h-96 border border-borderColour bg-mainTheme hover:shadow-xl">
                                     <figure>
                                         <img
                                             src={blog.details_image}
                                             alt="Shoes"
-                                            className="w-full h-[200px] object-cover" />
+                                            className="w-full h-[250px] overflow-hidden object-cover" />
                                     </figure>
                                     <div className="p-5">
-                                        <div className="flex justify-start items-center space-x-2">
+                                        <div className="flex justify-start items-center space-x-2 mb-1">
                                             <div className="flex items-center justify-start">
                                                 <div className="avatar">
                                                     <div className="w-8 rounded">
@@ -46,14 +44,16 @@ const CategoryBlogs = () => {
                                                 </div>
                                             </div>
                                             <div>
-                                                <p>{blog.owner_name}</p>
+                                                <p className="text-sm font-medium text-black">{blog.owner_name}</p>
                                             </div>
                                         </div>
-
-                                        <p className="my-3">{blog.short_description}</p>
-                                        <div className="flex flex-row justify-between self-end">
+                                        <Link to={`/blogdetails/${blog._id}`}>
+                                            <h1 title="See Details" className="font-bold text-md">{blog.title}</h1>
+                                        </Link>
+                                        <p className="mb-2 text-sm font-medium text-black">{blog.short_description}</p>
+                                        <div className="flex flex-row justify-between mt-5 self-end">
                                             <div className="flex flex-row space-x-2">
-                                               <p className="text-sm text-textSmallGray font-medium md:block">{blog.date}</p>
+                                                <p className="text-sm text-textSmallGray font-medium">{blog.date}</p>
                                                 <div className="flex space-x-1">
                                                     <LikeFunctionality id={blog._id}></LikeFunctionality>
                                                     <span className="text-sm text-textSmallGray font-medium ml-1">
