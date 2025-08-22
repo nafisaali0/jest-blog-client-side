f229ef83466c97e2dc446f74ebcd58b7a23e3ed5
nafisaali0 <nafisaali20006@gmail.com>
2025-08-22 17:12:17 +0600

    change recent blog and top blog function

--- src/components/shared/TabHome/TabHome.jsx
+++ src/components/shared/TabHome/TabHome.jsx
@@ -13,51 +13,59 @@ const TabHome = () => {
     const [comments] = useComment();
     const [sortedBlogs, setSortedBlogs] = useState([]);//for top blogs
     const [recentBlogs, setRecentBlogs] = useState([])//show category from backend
-    const [sortDateTimeBlogs, setSortDateTimeBlogs] = useState([]) //load sorting blogs
     const [displayCount, setDisplayCount] = useState(6);//for all blogs btn
     const [displayCountSix, setDisplayCountSix] = useState(6);//for recent blogs btn
-    const [displayCountTopBg, setDisplayCountTopBg] = useState(6);//for recent blogs btn
+    // const [displayCountTopBg, setDisplayCountTopBg] = useState(6);
     const [selectedTab, setSelectedTab] = useState(0);//for Tab list
 
     useEffect(() => {
-        fetch('https://blog-server-side-ochre.vercel.app/blogs')
-            .then(res => res.json())
-            .then(data => setRecentBlogs(data))
+        if (!blogs.length) return;
 
-    }, [])
-
-    // sorting by data and time 
-    useEffect(() => {
-        const sortedDateTime = recentBlogs.sort((a, b) =>
-            moment(b.date + ' ' + b.time, 'MMM Do YY LT').toDate() -
-            moment(a.date + ' ' + a.time, 'MMM Do YY LT').toDate()
+        const sorted = [...blogs].sort((a, b) =>
+            moment(b.date + " " + b.time, "MMM Do YY LT").toDate() -
+            moment(a.date + " " + a.time, "MMM Do YY LT").toDate()
         );
-        setSortDateTimeBlogs(sortedDateTime)
-    }, [recentBlogs])
+        setRecentBlogs(sorted);
+    }, [blogs]);
 
     //top Blogs
+    // useEffect(() => {
+    //     if (comments.length > 0 && blogs.length > 0) {
+
+    //         // Step 2: Count the comments for each blog
+    //         const commentCountByBlog = comments.reduce((acc, comment) => {
+    //             acc[comment.blog_id] = (acc[comment.blog_id] || 0) + 1;
+    //             return acc;
+    //         }, {});
+
+    //         // Step 3: Merge blog information with comment counts
+    //         const blogsWithCommentCount = blogs.map(blog => ({
+    //             ...blog,
+    //             count: commentCountByBlog[blog._id] || 0
+    //         }));
+
+    //         // Step 4: Sort the array by comment count in descending order
+    //         blogsWithCommentCount.sort((a, b) => b.count - a.count);
+
+    //         // Step 5: Update the state with the sorted blogs
+    //         setSortedBlogs(blogsWithCommentCount);
+    //     }
+    // }, [comments, blogs]);
+
+    // new:Top blog, that only give top comments blog.
     useEffect(() => {
-        if (comments.length > 0 && blogs.length > 0) {
+        if (!blogs.length || !comments.length) return;
 
-            // Step 2: Count the comments for each blog
-            const commentCountByBlog = comments.reduce((acc, comment) => {
-                acc[comment.blog_id] = (acc[comment.blog_id] || 0) + 1;
-                return acc;
-            }, {});
-
-            // Step 3: Merge blog information with comment counts
-            const blogsWithCommentCount = blogs.map(blog => ({
+        const sorted = [...blogs]
+            .map(blog => ({
                 ...blog,
-                count: commentCountByBlog[blog._id] || 0
-            }));
+                commentCount: comments.filter(c => c.blog_id === blog._id).length,
+            }))
+            .sort((a, b) => b.commentCount - a.commentCount)
+            .slice(0, 5);
 
-            // Step 4: Sort the array by comment count in descending order
-            blogsWithCommentCount.sort((a, b) => b.count - a.count);
-
-            // Step 5: Update the state with the sorted blogs
-            setSortedBlogs(blogsWithCommentCount);
-        }
-    }, [comments, blogs]);
+        setSortedBlogs(sorted);
+    }, [blogs, comments]);
 
     const handleSeeAll = () => {
         setDisplayCount(displayCount + 6);
@@ -65,9 +73,9 @@ const TabHome = () => {
     const handleSeeAllRecent = () => {
         setDisplayCountSix(displayCountSix + 6);
     }
-    const handleSeeAllTopBlog = () => {
-        setDisplayCountTopBg(displayCountTopBg + 6);
-    }
+    // const handleSeeAllTopBlog = () => {
+    //     setDisplayCountTopBg(displayCountTopBg + 6);
+    // }
     const handleTabSelect = (index) => {
         setSelectedTab(index);
     };
@@ -99,7 +107,7 @@ const TabHome = () => {
                     <TabPanel>
                         <div className="grid gap-5">
                             {
-                                sortDateTimeBlogs.slice(0, displayCountSix).map(blog =>
+                                recentBlogs.slice(0, displayCountSix).map(blog =>
                                     <ShowRecentBlog
                                         key={blog._id}
                                         blog={blog}>
@@ -107,24 +115,30 @@ const TabHome = () => {
                                 )
                             }
                         </div>
-                        {displayCountSix <= sortDateTimeBlogs.length &&
+                        {displayCountSix <= recentBlogs.length &&
                             <button className="buttonView mt-5 font-semibold" onClick={handleSeeAllRecent}>View More</button>
                         }
                     </TabPanel>
                     <TabPanel>
                         <div className="grid gap-5">
                             {
-                                sortedBlogs.slice(0, displayCountTopBg).map(blog =>
+                                sortedBlogs.map(blog =>
                                     <ShowTopBlogs
                                         key={blog._id}
                                         blog={blog}>
                                     </ShowTopBlogs>
                                 )
+                                // sortedBlogs.slice(0, displayCountTopBg).map(blog =>
+                                //     <ShowTopBlogs
+                                //         key={blog._id}
+                                //         blog={blog}>
+                                //     </ShowTopBlogs>
+                                // )
                             }
                         </div>
-                        {displayCountTopBg <= sortedBlogs.length &&
+                        {/* {displayCountTopBg <= sortedBlogs.length &&
                             <button className="buttonView mt-5 font-semibold" onClick={handleSeeAllTopBlog}>View More</button>
-                        }
+                        } */}
                     </TabPanel>
                 </Tabs>
             </div>
