af77cab823707d19267b6d16071aff271eccb78a
nafisaali0 <nafisaali20006@gmail.com>
2025-09-03 03:58:40 +0600

    add toast in Wishlish func

--- src/components/Functionality/SaveBlogsFunctionality/SaveBlogsFunctionality.jsx
+++ src/components/Functionality/SaveBlogsFunctionality/SaveBlogsFunctionality.jsx
@@ -1,28 +1,37 @@
 import { useContext } from "react"
-import Swal from "sweetalert2"
 import { AuthContext } from "../../../Providers/AuthProvider"
 import { PropTypes } from 'prop-types';
 import useWishLIst from "../../../hooks/useWishList";
 import { BsBookmarkCheck } from "react-icons/bs";
+import { Slide, toast } from "react-toastify";
 
 
 const SaveBlogsFunctionality = ({ blog }) => {
+
     const { user, } = useContext(AuthContext)
     const [wishList] = useWishLIst();
     const { _id, title, details_image, short_description, category, long_description, date, time, owner_name, owner_image, owner_Email } = blog
 
     const handleWishList = () => {
 
-        const email = user.email
+        const email = user?.email
         const blogId = _id
 
         const blogWishListInfo = { blogId, email, title, short_description, long_description, details_image, date, time, category, owner_name, owner_image, owner_Email }
-        const checkSameBlog = wishList.some(readBlog => readBlog.blogId === blogId)
+        const checkSameBlog = wishList?.some(readBlog => readBlog?.blogId === blogId)
 
         if (checkSameBlog === true) {
-            Swal.fire(
-                'Already Added in Reading List'
-            )
+            toast.info('Already in Reading List', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: true,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: 1,
+                theme: "light",
+                transition: Slide,
+            });
         } else {
             //send wishlist data to the server and below link to backend's created API and load in mongo as DB
             fetch('https://blog-server-side-ochre.vercel.app/wishlist', {
@@ -36,9 +45,17 @@ const SaveBlogsFunctionality = ({ blog }) => {
                 .then(data => {
                     console.log(data)
                     if (data.insertedId) {
-                        Swal.fire(
-                            'Added Blog to Reading List'
-                        )
+                        toast.success('Saved to Reading List!', {
+                            position: "top-right",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: false,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: 1,
+                            theme: "light",
+                            transition: Slide,
+                        });
                     }
                 })
         }
--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import "aos/dist/aos.css";
 import { RouterProvider } from "react-router-dom";
 import router from './Routes/Routes';
 import AuthProvider from './Providers/AuthProvider';
+import 'react-toastify/dist/ReactToastify.css'
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { ToastContainer } from 'react-toastify';
 const queryClient = new QueryClient()
 
 
@@ -22,6 +24,19 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          // transition:Slide,
+        />
       </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>,
